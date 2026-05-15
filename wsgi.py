7b6ac902954eca977350fb8a0f3e76e6a50d4565
Obisesan Israel <israelobisesan462@gmail.com>
2026-05-15 17:01:01 +0100

    add env vars to wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,10 @@
-import sys
 import os
 
-path = os.path.dirname(os.path.abspath(__file__))
+os.environ["FIREBASE_KEY_PATH"] = "serviceAccountKey.json"
+os.environ["NEWSDATA_API_KEY"] = "pub_6fa311d44ab545f289e80f78441c3d7c"
+
+import sys
+path = "/home/israel00/news-guy"
 if path not in sys.path:
     sys.path.insert(0, path)
 
